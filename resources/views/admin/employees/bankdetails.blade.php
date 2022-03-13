4fbb5c6a2c29848329f8ed702cb2edbe8af5db0d
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-03-13 16:36:55 +0600

    wip

--- resources/views/admin/employees/bankdetails.blade.php
+++ resources/views/admin/employees/bankdetails.blade.php
@@ -12,60 +12,12 @@
                         <div class="card-title">
                             <h3 class="card-label">Bank Details</h3>
                         </div>
-                        <div class="card-toolbar">
-                            <!--begin::Button-->
-                            {{--                            @can('create', \App\Models\Admin::class)--}}
-{{--                            <a href="{{route('admin.bankdetails.create')}}" class="btn btn-success">--}}
-                                <i class="flaticon2-plus-1"></i> Add New
-                            </a>
-                        {{--                        @endcan--}}
-                        <!--end::Button-->
-                        </div>
+
                     </div>
                     <div class="card-body">
-                        <table class="table table-bordered">
-                            <thead >
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Employee ID</th>
-                                <th scope="col">Profile Image</th>
-                                <th scope="col">Full Name</th>
-                                <th scope="col">Gender</th>
-                                <th scope="col">Mobile Number</th>
-                                <th scope="col" style="width: 10%">Date of birth</th>
-                                <th scope="col">Designation</th>
-                                <th scope="col">Joining Date</th>
-                                <th scope="col">Local Address</th>
-                                <th scope="col">Permanent Address</th>
-                                <th scope="col">Status</th>
-                            </tr>
-                            </thead>
 
-                            <tbody>
-{{--                            @foreach($employees as $employee)--}}
-{{--                                <tr>--}}
-{{--                                    <td> {{$employee->id }} </td>--}}
-{{--                                    <td> {{$employee->employeeID }} </td>--}}
-{{--                                    <td> {{$employee->profileImage }} </td>--}}
-{{--                                    <td> {{$employee->fullName }} </td>--}}
-{{--                                    <td> {{$employee->gender }} </td>--}}
-{{--                                    <td> {{$employee->mobileNumber }} </td>--}}
-{{--                                    <td > {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $employee->date_of_birth)->format('d-m-Y') }} </td>--}}
-{{--                                    <td> {{$employee->designation }} </td>--}}
-{{--                                    <td> {{$employee->joiningDate }} </td>--}}
-{{--                                    <td> {{$employee->localAddress }} </td>--}}
-{{--                                    <td> {{$employee->permanentAddress }} </td>--}}
-{{--                                    <td> <span class="badge badge-success">{{$employee->status == 0 ? '' : 'Active' }} </span>--}}
-{{--                                        <span class="badge badge-danger">{{$employee->status == 1 ? '' : 'Inactive' }}</span>--}}
-{{--                                    </td>--}}
-{{--                                </tr>--}}
-{{--                            @endforeach--}}
-                            </tbody>
-                        </table>
+//details of bank
 
-                        <div class="d-flex justify-content-center">
-{{--                            {!! $employees->links() !!}--}}
-                        </div>
                     </div>
                 </div>
                 <!--end::Card-->
