54986c8f315ac4596d08d1762dc16e8011f6987f
NahinAhmed28 <nahin.ahmed28@gmail.com>
2022-04-05 20:15:31 +0600

    wip-home

--- routes/employee.php
+++ routes/employee.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Employee\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('employee')->name('employee.')->middleware([''])->group(function () {
+Route::prefix('employee')->name('employee.')->group(function () {
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 });
